edd0bcf379f7851a2f530a9f43840b2c77a896a2
ruwanswarna <ruwanswarna@gmail.com>
2026-01-05 11:21:23 +0530

    one to many, update product and modify references in User

--- routes/product.js
+++ routes/product.js
@@ -39,23 +39,54 @@ router.put("/:productId", async (req, res) => {
 	const productId = req.params.productId;
 	const { name, image, user } = req.body;
 	try {
-        const productData = await Product.findById(productId);
-        //previous user = new user
-            productData.name = name;
-            productData.image = image;
-        ////
-        if(productData.user===user){
-            
-            await productData.save();
-            return res.status(200).json({ success: true, error: null, data: productData });
-        }
-        await User.updateOne({_id: productData.user}, {$pull: {products: productData._id}});
-        await User.updateOne({_id: user}, {$push: {products: productId}});
-        
+		const productData = await Product.findById(productId);
+		//previous user = new user
+		productData.name = name;
+		productData.image = image;
+		////
+		if (productData.user === user) {
+			await productData.save();
+			return res
+				.status(200)
+				.json({ success: true, error: null, data: productData });
+		}
+
+		//else// 
+		productData.user = user;
+		await productData.save();
+
+		await User.updateOne(
+			{ _id: productData.user },
+			{ $pull: { products: productData._id } }
+		); //remove product from previous user
+		await User.updateOne({ _id: user }, { $push: { products: productId } }); //add edited product to the new user
+		res.status(200).json({ success: true, error: null, data: productData });
 	} catch (error) {
 		console.log(error);
 		res.status(500).send("Internal  server error");
 	}
 });
 
+//delete a product
+router.delete("/:productId", async (req, res) => {
+	const productId = req.params.productId;
+	try {
+		const productData = await Product.findByIdAndDelete(productId);
+		if (!productData) {
+			return res
+				.status(404)
+				.json({ success: false, error: "product does not exist", data: null });
+		}
+		await User.updateOne(
+			{ _id: productData.user },
+			{ $pull: { products: productData._id } }
+		);
+		res
+			.status(200)
+			.json({ success: true, error: null, data: "product deleted" });
+	} catch (error) {
+		console.log(error);
+		res.status(500).send("Internal  server error");
+	}
+});
 export default router;
